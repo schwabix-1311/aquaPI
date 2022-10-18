e4aa45e1ad77be3005552c0d649d9d72def6f5af
Schwabix-1311 <git@kuhn-clan.de>
2022-10-18 22:26:50 +0200

    IoRegistry including the driver_factory method
    Not yet tested on a real Raspi, thus the automatic port scanning in the
    drivers may have bugs.
    Loadable custom drivers.
    This is the base for easy re-config of driver ports.

---
+++ aquaPi/driver/DriverPWM.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+import os
+import random
+try:
+    import RPi.GPIO as GPIO
+except:
+    pass
+
+from .base import *
+
+
+log = logging.getLogger('DriverPWM')
+log.brief = log.warning  # alias, warning is used as brief info, level info is verbose
+
+log.setLevel(logging.WARNING)
+# log.setLevel(logging.INFO)
+# log.setLevel(logging.DEBUG)
+
+
+# ========== PWM ==========
+
+
+class DriverPWMbase(Driver):
+    """ abstrract base of PWM drivers
+    """
+    def __init__(self, func, cfg):
+        if func not in [PortFunc.PWM]:
+            raise DriverParamError('This driver supports PWM, nothing else.')
+
+        super().__init__(func, cfg)
+        self._channel = int(cfg['channel'])
+        self._pin = int(cfg['pin'])
+
+
+class DriverPWM(DriverPWMbase):
+    """ one PWM channel, hardware PWM
+    """
+    @staticmethod
+    def find_ports():
+        if not is_raspi() or True:
+            # name: IoPort('function', 'driver', 'cfg')
+            io_ports = { 'PWM 0':  IoPort(PortFunc.PWM, DriverPWM, {'pin': 18, 'channel': 0, 'fake': True})
+                       , 'PWM 1':  IoPort(PortFunc.PWM, DriverPWM, {'pin': 19, 'channel': 1, 'fake': True}) }
+        else:
+            io_ports = {}
+            cnt = 0
+            for pin in range(28):
+                try:
+                    func = PinFunc(GPIO.gpio_function(pin))
+                    if func in [PinFunc.PWM]:
+                        port_name = 'PWM %d' % cnt
+                        io_ports[port_name] = IoPort(PortFunc.PWM, DriverPWM, {'pin': pin, 'channel': cnt})
+                        cnt += 1
+                    else:
+                        log.debug('pin %d is in use as %s' % (pin, func.name))
+                except:
+                    log.debug('Unknown function on pin %d = %d' % (pin, GPIO.gpio_function(pin)))
+        return io_ports
+
+    def __init__(self, func, cfg):
+        super().__init__(func, cfg)
+
+        self.name = 'PWM %d @ pin %d' % (self._channel, self._pin)
+        if not self._fake:
+            self._sysfs = path.join('/sys/class/pwm/pwmchip0/pwm%d/' % self._channel)
+            self.name = 'PWM %d @ sysfs' % self._channel
+            with open(path.join(self._sysfs, 'enable'), 'wt') as p:
+                p.write('0')
+            with open(path.join(self._sysfs, 'period'), 'wt') as p:
+                p.write('3333333')
+        else:
+            self.name = '!' + self.name
+
+        self.write(0)
+
+    def close(self):
+        if not self._fake:
+            with open(path.join(self._sysfs, 'enable'), 'wt') as p:
+                p.write('0')
+
+    def write(self, val):
+        log.info('%s -> %f' % (self.name, float(val)))
+        if not self._fake:
+            with open(path.join(self._sysfs, 'duty_cycle'), 'wt') as p:
+                p.write('%d' % int(val / 100.0 * 3333333))
+            with open(path.join(self._sysfs, 'enable'), 'wt') as p:
+                p.write('1' if val > 0 else '0')
+        else:
+            self._val = float(val)
+
+
+if False:
+    class DriverSoftPWM(DriverPWMbase):
+        """ one PWM channel, hardware PWM
+            This is problematic, as it uses GPIO ports, thus should be handled by DriverGPIO, but then
+            the PortFunc.IO is unexpected. Make PortFunc a set of PortFuncs and have a PortFunc.SoftPWM ?
+        """
+        @staticmethod
+        def find_ports():
+            if not is_raspi() or True:
+                # name: IoPort('function', 'driver', 'cfg')
+                io_ports = { 'softPWM 0':  IoPort(PortFunc.PWM, DriverSoftPWM, {'pin': 24, 'channel': 0, 'fake': True})
+                           , 'softPWM 1':  IoPort(PortFunc.PWM, DriverSoftPWM, {'pin': 25, 'channel': 1, 'fake': True}) }
+            else:
+                io_ports = {}
+                cnt = 0
+                for pin in range(28):
+                    try:
+                        func = PinFunc(GPIO.gpio_function(pin))
+                        if func in [PinFunc.PWM]:
+                            port_name = 'PWM %d' % cnt
+                            io_ports[port_name] = IoPort(PortFunc.PWM, DriverPWM, {'pin': pin, 'channel': cnt})
+                            cnt += 1
+                        else:
+                            log.debug('pin %d is in use as %s' % (pin, func.name))
+                    except:
+                        log.debug('Unknown function on pin %d = %d' % (pin, GPIO.gpio_function(pin)))
+            return io_ports
+
+        def __init__(self, func, cfg):
+            super().__init__(func, cfg)
+
+            self.name = 'SoftPWM %d @ pin %d' % (self._channel, self._pin)
+            if not self._fake:
+                GPIO.setup(self._pin, GPIO.OUT)
+                self._pwm = GPIO.PWM(self._pin, 300)
+                self._pwm.start(0)
+
+            if self._fake:
+                self.name = '!' + self.name
+
+            self.write(0)
+
+        def close(self):
+            if not self._fake:
+                self._pwm.stop()
+                GPIO.cleanup(self._pin)
+
+        def write(self, val):
+            log.info('%s -> %f' % (self.name, float(val)))
+            if not self._fake:
+                self._pwm.ChangeDutyCycle(float(val))
+            else:
+                self._val = float(val)
--- aquaPi/driver/base.py
+++ aquaPi/driver/base.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import logging
+import sys
 from os import path
+from enum import Enum
+from collections import namedtuple
 try:
     import RPi.GPIO as GPIO
 except:
@@ -25,143 +28,6 @@ def is_raspi():
         with open('/sys/firmware/devicetree/base/model', 'r') as f:
             model = f.readline()
     return 'raspberry' in model.lower()
-
-
-# ========== IO resource map ==========
-
-
-"""
-[ (driver,   addr,        port,    func, free) ]
-
-('GPIO',    '',           12,       OUT, False )   "GPIOout 12 - Relais"
-('GPIO',    '',           0/1/4-17, IO,  True )
-('Motor',   '',           (21,22),  OUT, False )   "H-Bridge - Dosierpumpe"
-('I2C',     'x47',        0,        OUT, False )   "IOext 0 - CO2 Ventil"
-('I2C',     'x47',        1-7,      IO,  True )    "IOext 0 - CO2 Ventil"
-('Shelly',  '192.16..',   0,        OUT, False )   "Shelly Plug - Heizer"
-
-('GPIO',    '',           20,       IN,  False )   "GPIOin 20 - Taster"
-('Shelly',  '192.16..',   1,        IN,  False )   "Shelly Temp.1"
-
-('PWM',     '/sys/...',   18,       PWM, False )   "H-PWM 0 - Licht"
-('PWM',     '/sys/...',   19,       PWM, True )    "H-PWM 1"
-('PWM',     '',           24,       PWM, True )    "S-PWM 2 - Lüfter"
-('PA9685',  'x7F',        15,       PWM, True )    "X-PWM 3"
-('TC420',   '??',         (3,4,5),  PWM, False )   "TC420 2 - RGB Licht"
-('Shelly',  '192.16..',   0,        PWM, False )   "Shelly Dimmer - Ambilight"
-
-('AD1115',  'x7E',        2,        ADC, True )    "ADC 3 - pH Sonde"
-('OneWire', '28-xx',      0,        ADC, False )   "Sens 1 - Wassertemperatur"
-
-Initial enumeration fills this map via static driver functions. 
-GPIO has 3 functions: undetermined IO, IN, OUT
-Drivers can provide entries for more than one function if they implement all their methods.
-Each channel/port/pin is one entry. We will need multi-channel drivers (RGB, motor).
-Creation of a driver instance reserves the entry, in case of GPIO (or soft-PWM) this may change function!
-Function to query avilable entries by function. 
-Function to allocte/release a set of entries. This must restore initial function.
-
-IoPort = namedtupel('IoPort', ['function', 'driver', 'address', 'port', 'free'])
-
-class IoManager(object):
-    _map = {}
-
-    def __init__(self):
-        # iterate all class imports from a module, then call each class' port enumerator
-        # https://stackoverflow.com/questions/7584418/iterate-the-classes-defined-in-a-module-imported-dynamically
-        for driver in ['DrvGPIO', 'OneWire']):
-            _map.update(driver.enumerate(...))
-"""
-
-
-_pins_available = None
-_pins_unused = None
-
-# index into this array is channel num
-# entries are  int - GPIO pin for Soft PWM
-#              '/sys/...' - sysfs path to Hard PWM
-# (future)     'pca9685@adr#1' - chip, I2C adr & channel
-_pwms_available = None
-_pwms_unused = None
-
-_pin_func_map = {-1: 'UNKNOWN', 0: 'OUT', 1: 'IN', 40: 'SERIAL', 41: 'SPI', 42: 'I2C', 43: 'PWM'}
-
-def _init_maps():
-    global _pins_available, _pins_unused
-    global _pwms_available, _pwms_unused
-
-    if not _pins_available:
-        if is_raspi():
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            _pins_available = set()
-            _pwms_available = []
-            pwm_cnt = 0
-            for i in range(28):
-                try:
-                    fnc = GPIO.gpio_function(i)
-                    if fnc in [0, 1]:
-                        _pins_available.add(i)
-                    elif fnc == 43:
-                        _pwms_available.append('/sys/class/pwm/pwmchip0/pwm%d/' % pwm_cnt)
-                        #soft-PWM _pwms_available.append(i)
-                        pwm_cnt += 1
-                    else:
-                        log.info('pin %d is in use as %s' % (i, _pin_func_map[fnc]))
-                except:
-                    log.debug('func pin %d = %d' % (i, fnc))
-        else:
-            # not a Raspi, simulate typical IO layout, skip I²C and PWM
-            _pins_available = {0, 1, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23, 24, 25, 26, 27}
-            _pwms_available = [18, 19]
-
-        log.brief('available GPIO pins: %r' % _pins_available)
-        log.brief('available PWM channels: %r' % _pwms_available)
-        _pins_unused = set(_pins_available)
-        _pwms_unused = set(range(len(_pwms_available)))
-
-
-def get_unused_pins():
-    _init_maps()
-    return _pins_unused
-
-
-def assign_pin(pin, used):
-    global _pins_available, _pins_unused
-
-    _init_maps()
-    # pin must be a member of set _pins_available
-    if not pin in _pins_available:
-        raise DriverInvalidPortError(port=pin)
-    if used and not pin in _pins_unused:
-        raise DriverPortInuseError(port=pin)
-
-    if used:
-        _pins_unused.remove(pin)
-    else:
-        _pins_unused.add(pin)
-
-
-def get_unused_pwms():
-    _init_maps()
-    return _pwms_unused
-
-
-def assign_pwm(channel, used):
-    global _pwms_available, _pwms_unused
-
-    _init_maps()
-    # channel must be an index into array pwms_available
-    if channel < 0 or channel >= len(_pwms_available):
-        raise DriverInvalidAddrError(adr=channel)
-    if used and not channel in _pwms_unused:
-        raise DriverPortInuseError(port=channel)
-
-    if used:
-        _pwms_unused.remove(channel)
-    else:
-        _pwms_unused.add(channel)
-    return _pwms_available[channel]
 
 
 # ========== Exceptions ==========
@@ -205,22 +71,13 @@ class Driver:
         __getstate__/__setstate__ overloads in derived classes.
     """
     # TODO this persistance approach could be transferred to MsgNodes!
-    def __init__(self, cfg):
+    def __init__(self, func, cfg):
         self.name = '!abstract'
+        self.func = None
         self.cfg = cfg
         self._fake = not is_raspi()
         if 'fake' in cfg:
             self._fake |= bool(cfg['fake'])
-
-    def __getstate__(self):
-        state = {'name': self.name, 'cfg': self.cfg}
-        log.debug('Driver.getstate %r', state)
-        return state
-
-    def __setstate__(self, state):
-        log.debug('Driver.setstate %r', state)
-        self.__init__(state['cfg'])
-        self.name = state['name']
 
     def __str__(self):
         return '{}({})'.format(type(self).__name__, self.cfg)
@@ -230,8 +87,8 @@ class InDriver(Driver):
     """ base class of all input drivers
         InDriver can be read, e.g. a temperature sensor.
     """
-    def __init__(self, cfg):
-        super().__init__(cfg)
+    def __init__(self, func, cfg):
+        super().__init__(func, cfg)
         self.name = '!abstract IN'
         self._interval = 0
 
@@ -243,8 +100,8 @@ class OutDriver(Driver):
     """ base of all output drivers
         OutDriver can be written, the last written value can be read.
     """
-    def __init__(self, cfg):
-        super().__init__(cfg)
+    def __init__(self, func, cfg):
+        super().__init__(func, cfg)
         self.name = '!abstract OUT'
         self._state = 0
 
@@ -253,3 +110,124 @@ class OutDriver(Driver):
 
     def read(self):
         return self._state
+
+
+# ========== IO resource map ==========
+
+
+"""
+example
+
+"GPIO 0..xx": { IO,  DriverGPIO,   {pin: 0..x} }         - unused, func IO -> OUT/IN, x entries
+"IOext 1..7": { IO,  DriverPCFxx,  {addr: 0x47, ch: x} } - unused, x entries
+
+"GPIO 12":    { OUT, DriverGPIO,   {pin: 12} }           - Relais
+"IOext 0":    { OUT, DriverPCFxx,  {addr: 0x47, ch: 0} } - CO2 Ventil
+"ShellyPlug1":{ OUT, DriverShelly, {ip: '192..', ch:0} } - S.Plug - Heizer
+"H-Bridge 1": { OUT, Drivermotor,  {pins: (21,22)} }     - Dosierpumpe
+
+"GPIO 20":    { IN,  DriverGPIO,   {pin: 20} }           - Taster
+"ShellyTemp1":{ IN,  DriverShelly, {ip: '192..', ch:2} } - S.Temp1
+
+"PWM 0":      { PWM, DriverPWM,    {ch: 0, pin: 18} }    - Licht"
+"PWM 1":      { PWM, DriverPWM,    {ch: 1, pin: 19} }    -
+"S-PWM 2":    { IO/PWM, DriverGPIO, {pin: 24} }          - Lüfter"   GPIO conflict!
+"PWMext 0-15":{ PWM, DriverPA9685, {addr:0x7F, ch:0..} } -
+"TC420 1":    { PWM, DriverTC420,  {usb:id, ch:1} }      - Mondlicht
+"TC420 2":    { PWM, DriverTC420,  {usb:id, ch:(3,4,5)}} - RGB Licht
+"ShellyDim":  { PWM, DriverShelly, {ip: '192..', ch:X} } - Ambilight
+
+"Sens 1":     { ADC, DriverOneWr,  {id:'28-xx'} }        - Wassertemperatur"
+"ADC 3":      { ADC, DriverAD1115, {addr:0x7E, ch:3} }   - pH Sonde"
+
+Initial enumeration fills this map via static driver functions.
+GPIO has 3 functions: IO (=undetermined), IN, OUT. or 4th: S-PWM?
+Drivers can provide entries for more than one function if they implement all their methods.
+Each channel/port/pin is one entry. Dict cfg is driver's private property!
+Creation of a driver instance reserves the entry, in case of GPIO (or soft-PWM) this may change function!
+get_ports_by_function() returns a view on avialbale or used IoPorts.
+driver_factory(key,func) creates a driver for specified port and function.
+driver_destruct(key) returns IoPort to unused. This must restore initial function.
+
+Multi-port drivers (RGB, motor) will need a dedicated factory method; very likely getting a higher level driver and a tupel of IoPorts.
+"""
+
+IoPort = namedtuple('IoPort', ['function', 'driver', 'cfg'])
+
+class PortFunc(Enum):
+    IO, IN, OUT, PWM, ADC = range(1, 6)
+
+class PinFunc(Enum):
+    UNKNOWN = -1
+    OUT = 0
+    IN = 1
+    SERIAL = 40
+    SPI = 41
+    I2C = 42
+    PWM = 43
+
+
+class IoRegistry(object):
+    _map = {}
+    _inuse = {}
+
+    def __init__(self):
+        # iterate all class imports from a module, then call each class' port enumerator
+        # https://stackoverflow.com/questions/7584418/iterate-the-classes-defined-in-a-module-imported-dynamically
+        # https://stackoverflow.com/questions/4821104/dynamic-instantiation-from-string-name-of-a-class-in-dynamically-imported-module
+
+        # This may be a hack, although it is documented on python.org for importlib.
+        # We load all modules Driver*.py in specific folders, then lookup all
+        # descendants of class Driver found in the module dicts.
+        # Those with a method find_ports() can report "their" IoPorts to IoRegistry.
+        # May be there's a much simpler ways to achieve the same though.
+
+        # all driver modules are dynamically imported in __init__.py and added to sys.modules
+        drv_mod_names = [mod for mod in sys.modules if 'driver.' in mod]
+        drv_classes = set()
+
+        # collect them in a set to avoid duplicates
+        for name in drv_mod_names:
+            drv_module = sys.modules[name]
+            log.debug('# module %s', drv_module)
+            dict_classes = [cl for cl in drv_module.__dict__.values() if type(cl) is type]
+            for mod_cl in dict_classes:
+                if issubclass(mod_cl, Driver):
+                    log.debug('found drv class %r', mod_cl)
+                    drv_classes.add(mod_cl)
+
+        for drv in drv_classes:
+            if hasattr(drv, 'find_ports'):
+                drv_ports = drv.find_ports()
+                log.debug('driver %r reported %r', drv, drv_ports)
+                # TODO should reject duplicate ports, same port should in theory not be reported
+                #      by multiple drivers, but better play safe
+                IoRegistry._map.update(drv_ports)
+        print('%r', IoRegistry._map)
+
+    def get_ports_by_function(self, funcs, free=True):
+        mp = IoRegistry._map if free else IoRegistry._inuse
+        # return [key for key in mp if mp[key].function in funcs]
+        return {key: mp[key] for key in mp if mp[key].function in funcs}
+
+    def driver_factory(self, port, func):
+        """ Create a driver for a single port.
+            Parameter func requests the function for ports that support alternatives, e.g. IO -> In or OUT
+            Drivers that use >1 port are created by a dedicated factory (later)
+        """
+        log.debug('create a driver for %r - %r', port, func)
+        if port in IoRegistry._inuse.keys():
+            raise DriverPortInuseError(port=port)
+        if port not in IoRegistry._map.keys():
+            raise DriverParamError('There is no port named %s' % port)
+
+        try:
+            io_port = IoRegistry._map[port]
+            driver = io_port.driver(func, io_port.cfg)
+
+            IoRegistry._inuse.update(port = IoPort(func, io_port.driver, io_port.cfg))
+            del IoRegistry._map[port]
+        except Exception as ex:
+            #TODO report error, e.g.
+            log.exception('Failed to create driver:' + port)
+        return driver
